9e2dd7401c00dac8ecf8a127ec411b3984255072
BSzeling <114461188+BSzeling@users.noreply.github.com>
2025-04-29 02:11:36 +0800

    Update settings.py

--- reborn/reborn/settings.py
+++ reborn/reborn/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'rebornApp',
 ]
 
+AUTH_USER_MODEL = 'rebornApp.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
