99c1b329b214f49457bb8115532cd418ba79bfa0
BSzeling <114461188+BSzeling@users.noreply.github.com>
2025-04-30 04:21:27 +0800

    Update urls.py

--- reborn/rebornApp/urls.py
+++ reborn/rebornApp/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from . import views 
+from . import views
+
+app_name = 'Reborn'
 
 urlpatterns = [
-    path('', views.home, name = 'home'),
+    path('seller/dashboard/', views.seller_dashboard, name='seller_dashboard'),
+    path('seller/add/', views.add_item, name='add_item'),
+    path('seller/edit/<int:item_id>/', views.edit_item, name='edit_item'),
+    path('seller/sold/<int:item_id>/', views.mark_item_sold, name='mark_item_sold'),
 ]
