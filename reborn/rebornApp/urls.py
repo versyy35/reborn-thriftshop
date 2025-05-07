d53e3aabd0353fb3e10781c50ca5d15f5d29f777
BSzeling <114461188+BSzeling@users.noreply.github.com>
2025-05-07 11:27:49 +0800

    Revert "Update urls.py"
    This reverts commit 99c1b329b214f49457bb8115532cd418ba79bfa0.

--- reborn/rebornApp/urls.py
+++ reborn/rebornApp/urls.py
@@ -1,11 +1,6 @@
 from django.urls import path
-from . import views
-
-app_name = 'Reborn'
+from . import views 
 
 urlpatterns = [
-    path('seller/dashboard/', views.seller_dashboard, name='seller_dashboard'),
-    path('seller/add/', views.add_item, name='add_item'),
-    path('seller/edit/<int:item_id>/', views.edit_item, name='edit_item'),
-    path('seller/sold/<int:item_id>/', views.mark_item_sold, name='mark_item_sold'),
+    path('', views.home, name = 'home'),
 ]
