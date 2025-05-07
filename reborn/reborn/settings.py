c1f274ceb6351253bfc5a94b6ce6e5a7a91f3a4d
BSzeling <114461188+BSzeling@users.noreply.github.com>
2025-05-07 11:26:48 +0800

    Revert "Update settings.py"
    This reverts commit 9e2dd7401c00dac8ecf8a127ec411b3984255072.

--- reborn/reborn/settings.py
+++ reborn/reborn/settings.py
@@ -42,8 +42,6 @@ INSTALLED_APPS = [
     'rebornApp',
 ]
 
-AUTH_USER_MODEL = 'rebornApp.User'
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
